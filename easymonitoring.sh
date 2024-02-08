6e286200af69a7e680b5635f3c0bbbea2e47ee58
Ralph Balazs <rbalazs@bess-f4r6s2.clusters.42paris.fr>
2024-02-08 17:48:02 +0100

    EasyMonitoring.sh

--- easymonitoring.sh
+++ easymonitoring.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
-
 ARCH=$(uname -srvmo)
-PCPU=$(grep 'physical id' /proc/cpuinfo | uniq | wc -l)
-VCPU=$(grep processor /proc/cpuinfo | uniq | wc -l)
-RAM_TOTAL=$(free -h | grep Mem | awk '{print $2}')
-RAM_USED=$(free -h | grep Mem | awk '{print $3}')
-RAM_PERC=$(free -k | grep Mem | awk '{printf("%.2f%%"), $3 / $2 * 100}')
-DISK_TOTAL=$(df -h --total | grep total | awk '{print $2}')
-DISK_USED=$(df -h --total | grep total | awk '{print $3}')
-DISK_PERC=$(df -k --total | grep total | awk '{print $5}')
-CPU_LOAD=$(top -bn1 | grep '^%Cpu' | xargs | awk '{printf("%.1f%%"), $2 + $4}')
+PCPU=$(grep -c 'physical id' /proc/cpuinfo)
+VCPU=$(grep -c processor /proc/cpuinfo)
+RAM_TOTAL=$(free -h | awk '/^Mem/ {print $2}')
+RAM_USED=$(free -h | awk '/^Mem/ {print $3}')
+RAM_PERC=$(free | awk '/^Mem/ {printf("%.2f%%"), $3 / $2 * 100}')
+DISK_TOTAL=$(df -h --total | awk '/total/ {print $2}')
+DISK_USED=$(df -h --total | awk '/total/ {print $3}')
+DISK_PERC=$(df -h --total | awk '/total/ {print $5}')
+CPU_LOAD=$(top -bn1 | awk '/^%Cpu/ {printf("%.1f%%"), $2 + $4}')
 LAST_BOOT=$(who -b | awk '{print($3 " " $4)}')
-LVM=$(if [ $(lsblk | grep lvm | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
+LVM=$(lsblk -o TYPE | grep -q lvm && echo yes || echo no)
 TCP=$(grep TCP /proc/net/sockstat | awk '{print $3}')
 USER_LOG=$(who | wc -l)
 IP_ADDR=$(hostname -I | awk '{print $1}')
-MAC_ADDR=$(ip link show | grep link/ether | awk '{print $2}')
+MAC_ADDR=$(ip link show | awk '/link\/ether/ {print $2}')
 SUDO_LOG=$(grep COMMAND /var/log/sudo/sudo.log | wc -l)
 
 wall "
@@ -28,8 +26,10 @@ wall "
        CPU Load        : $CPU_LOAD
        Last Boot       : $LAST_BOOT
        LVM use         : $LVM
-       TCP Connections : $TCP established
+       TCP Connections : $TCP ESTABLISHED
        Users logged    : $USER_LOG
        Network         : $IP_ADDR ($MAC_ADDR)
        Sudo            : $SUDO_LOG commands used
        ------------------------------------------------"
+
+
